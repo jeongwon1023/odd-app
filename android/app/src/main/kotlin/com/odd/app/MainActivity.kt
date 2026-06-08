4f3c127be31acd46ce96440c4504c485d17d9129
jeongwon1023 <aabu01046@gmail.com>
2026-06-08 23:49:55 +0900

    iOS: Runner scheme 추가

--- android/app/src/main/kotlin/com/odd/app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.odd.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/services/data_service.dart
+++
@@ -1,2 +0,0 @@
-// 이 파일은 더 이상 사용되지 않습니다.
-// 데이터는 naver_place_service.dart (네이버 API) + cache_service.dart (로컬 캐시)로 대체되었습니다.
